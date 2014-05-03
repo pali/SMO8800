f0c408512bf729dd383a20f6c06691063b6c685d
Pali Rohár <pali.rohar@gmail.com>
2014-05-03 12:41:24 +0200

    Cleanup driver for upstream

--- smo8800.c
+++ smo8800.c
@@ -1,7 +1,8 @@
 /*
- *  smo8800.c - Dell Latitude ACPI SMO8800 freefall sensor driver
+ *  smo8800.c - Dell Latitude ACPI SMO8800/SMO8810 freefall sensor driver
  *
- *  Copyright (C) 2012 Sonal Santan < sonal DOT santan AT gmail DOT com >
+ *  Copyright (C) 2012 Sonal Santan <sonal.santan@gmail.com>
+ *  Copyright (C) 2014 Pali Rohár <pali.rohar@gmail.com>
  *
  *  This is loosely based on lis3lv02d driver.
  *
@@ -14,99 +15,77 @@
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#define DRIVER_NAME	"smo8800"
-#define pr_fmt(fmt)	DRIVER_NAME ": " fmt
+#define DRIVER_NAME "smo8800"
+#define pr_fmt(fmt) DRIVER_NAME ": " fmt
 
 #include <linux/kernel.h>
 #include <linux/module.h>
-#include <linux/types.h>
-#include <linux/version.h>
 #include <linux/acpi.h>
 #include <linux/interrupt.h>
 #include <linux/miscdevice.h>
-#include <acpi/acpi_bus.h>
-#include <acpi/acpi_drivers.h>
 
-struct smo8800 {
-	void			*bus_priv;
-	u32			irq;
-	atomic_t		count;       /* interrupt count after last read */
-	struct miscdevice	miscdev;     /* for /dev/freefall */
-	unsigned long		misc_opened; /* whether the device is open */
-	wait_queue_head_t	misc_wait;   /* Wait queue for the misc device */
+struct smo8800_device {
+	u32 irq;                     /* acpi device irq */
+	atomic_t count;              /* count after last read */
+	struct miscdevice miscdev;   /* for /dev/freefall */
+	unsigned long misc_opened;   /* whether the device is open */
+	wait_queue_head_t misc_wait; /* Wait queue for the misc dev */
+	struct device *dev;          /* acpi device */
 };
-
-static struct smo8800 smo8800_dev;
 
 static irqreturn_t smo8800_interrupt_quick(int irq, void *data)
 {
-	struct smo8800 *smo = data;
-	atomic_inc(&smo->count);
-	wake_up_interruptible(&smo->misc_wait);
+	struct smo8800_device *smo8800 = data;
+	atomic_inc(&smo8800->count);
+	wake_up_interruptible(&smo8800->misc_wait);
 	return IRQ_WAKE_THREAD;
 }
 
 static irqreturn_t smo8800_interrupt_thread(int irq, void *data)
 {
-	pr_debug("Detected free fall\n");
+	struct smo8800_device *smo8800 = data;
+	dev_info(smo8800->dev, "detected free fall\n");
 	return IRQ_HANDLED;
 }
 
-static acpi_status smo8800_get_resource(struct acpi_resource *resource, void *context)
+static acpi_status smo8800_get_resource(struct acpi_resource *resource,
+					void *context)
 {
-	if (resource->type == ACPI_RESOURCE_TYPE_EXTENDED_IRQ) {
-		struct acpi_resource_extended_irq *irq;
-		u32 *device_irq = context;
+	struct acpi_resource_extended_irq *irq;
 
-		irq = &resource->data.extended_irq;
-		*device_irq = irq->interrupts[0];
-	}
+	if (resource->type != ACPI_RESOURCE_TYPE_EXTENDED_IRQ)
+		return AE_OK;
 
-	return AE_OK;
+	irq = &resource->data.extended_irq;
+	if (!irq || !irq->interrupt_count)
+		return AE_OK;
+
+	*((u32 *)context) = irq->interrupts[0];
+	return AE_CTRL_TERMINATE;
 }
 
-static void smo8800_enum_resources(struct acpi_device *device)
+static u32 smo8800_get_irq(struct acpi_device *device)
 {
+	u32 irq = 0;
 	acpi_status status;
 
 	status = acpi_walk_resources(device->handle, METHOD_NAME__CRS,
-				     smo8800_get_resource, &smo8800_dev.irq);
-	if (ACPI_FAILURE(status))
-		pr_debug("Error getting resources\n");
-}
+				     smo8800_get_resource, &irq);
+	if (ACPI_FAILURE(status)) {
+		dev_err(&device->dev, "acpi_walk_resources failed\n");
+		return 0;
+	}
 
-static int smo8800_misc_open(struct inode *inode, struct file *file)
-{
-	struct smo8800 *smo_data = container_of(file->private_data,
-						struct smo8800, miscdev);
-
-	if (test_and_set_bit(0, &smo_data->misc_opened))
-		return -EBUSY; /* already open */
-
-	atomic_set(&smo_data->count, 0);
-	return 0;
-}
-
-static int smo8800_misc_release(struct inode *inode, struct file *file)
-{
-	struct smo8800 *smo_data = container_of(file->private_data,
-						struct smo8800, miscdev);
-
-	clear_bit(0, &smo_data->misc_opened); /* release the device */
-	return 0;
+	return irq;
 }
 
 static ssize_t smo8800_misc_read(struct file *file, char __user *buf,
 				 size_t count, loff_t *pos)
 {
-	struct smo8800 *smo_data = container_of(file->private_data,
-						struct smo8800, miscdev);
+	struct smo8800_device *smo8800 = container_of(file->private_data,
+					 struct smo8800_device, miscdev);
 
 	DECLARE_WAITQUEUE(wait, current);
 	u32 data;
@@ -116,13 +95,12 @@ static ssize_t smo8800_misc_read(struct file *file, char __user *buf,
 	if (count < 1)
 		return -EINVAL;
 
-	add_wait_queue(&smo_data->misc_wait, &wait);
+	add_wait_queue(&smo8800->misc_wait, &wait);
 	while (true) {
 		set_current_state(TASK_INTERRUPTIBLE);
-		data = atomic_xchg(&smo_data->count, 0);
-		if (data) {
+		data = atomic_xchg(&smo8800->count, 0);
+		if (data)
 			break;
-		}
 
 		if (file->f_flags & O_NONBLOCK) {
 			retval = -EAGAIN;
@@ -150,121 +128,126 @@ static ssize_t smo8800_misc_read(struct file *file, char __user *buf,
 
 out:
 	__set_current_state(TASK_RUNNING);
-	remove_wait_queue(&smo_data->misc_wait, &wait);
+	remove_wait_queue(&smo8800->misc_wait, &wait);
 
 	return retval;
 }
 
+static int smo8800_misc_open(struct inode *inode, struct file *file)
+{
+	struct smo8800_device *smo8800 = container_of(file->private_data,
+					 struct smo8800_device, miscdev);
+
+	if (test_and_set_bit(0, &smo8800->misc_opened))
+		return -EBUSY; /* already open */
+
+	atomic_set(&smo8800->count, 0);
+	return 0;
+}
+
+static int smo8800_misc_release(struct inode *inode, struct file *file)
+{
+	struct smo8800_device *smo8800 = container_of(file->private_data,
+					 struct smo8800_device, miscdev);
+
+	clear_bit(0, &smo8800->misc_opened); /* release the device */
+	return 0;
+}
+
 static const struct file_operations smo8800_misc_fops = {
-	.owner	 = THIS_MODULE,
-	.read	 = smo8800_misc_read,
-	.open	 = smo8800_misc_open,
+	.owner = THIS_MODULE,
+	.read = smo8800_misc_read,
+	.open = smo8800_misc_open,
 	.release = smo8800_misc_release,
 };
 
 static int smo8800_add(struct acpi_device *device)
 {
 	int err;
+	struct smo8800_device *smo8800;
+
 	if (!device)
 		return -EINVAL;
 
-	smo8800_dev.miscdev.minor	= MISC_DYNAMIC_MINOR;
-	smo8800_dev.miscdev.name	= "freefall";
-	smo8800_dev.miscdev.fops	= &smo8800_misc_fops;
-	init_waitqueue_head(&smo8800_dev.misc_wait);
-	if (misc_register(&smo8800_dev.miscdev)) {
-		pr_err("misc_register failed\n");
-		return -EINVAL;
+	smo8800 = devm_kzalloc(&device->dev, sizeof(*smo8800), GFP_KERNEL);
+	if (!smo8800) {
+		dev_err(&device->dev, "failed to allocate device data\n");
+		return -ENOMEM;
 	}
 
-	smo8800_dev.bus_priv = device;
-	atomic_set(&smo8800_dev.count, 0);
-	device->driver_data = &smo8800_dev;
-	err = 0;
+	smo8800->dev = &device->dev;
+	smo8800->miscdev.minor = MISC_DYNAMIC_MINOR;
+	smo8800->miscdev.name = "freefall";
+	smo8800->miscdev.fops = &smo8800_misc_fops;
 
-	smo8800_enum_resources(device);
+	init_waitqueue_head(&smo8800->misc_wait);
 
-	if (!smo8800_dev.irq) {
-		pr_err("No IRQ. Disabling /dev/freefall\n");
-		goto out;
+	err = misc_register(&smo8800->miscdev);
+	if (err) {
+		dev_err(&device->dev, "failed to register misc dev: %d\n", err);
+		return err;
 	}
 
-	pr_info("IRQ %d\n", smo8800_dev.irq);
+	atomic_set(&smo8800->count, 0);
+	device->driver_data = smo8800;
 
-	err = request_threaded_irq(smo8800_dev.irq, smo8800_interrupt_quick,
+	smo8800->irq = smo8800_get_irq(device);
+	if (!smo8800->irq) {
+		dev_err(&device->dev, "failed to obtain IRQ\n");
+		err = -EINVAL;
+		goto error;
+	}
+
+	err = request_threaded_irq(smo8800->irq, smo8800_interrupt_quick,
 				   smo8800_interrupt_thread,
 				   IRQF_TRIGGER_RISING | IRQF_ONESHOT,
-				   DRIVER_NAME, &smo8800_dev);
-
+				   DRIVER_NAME, smo8800);
 	if (err) {
-		pr_err("request_threaded_irq failed\n");
-		goto out;
+		dev_err(&device->dev,
+			"failed to request thread for IRQ %d: %d\n",
+			smo8800->irq, err);
+		goto error;
 	}
 
+	dev_info(&device->dev, "device /dev/freefall registered with IRQ %d\n",
+		 smo8800->irq);
 	return 0;
 
-out:
-	misc_deregister(&smo8800_dev.miscdev);
+error:
+	misc_deregister(&smo8800->miscdev);
 	return err;
 }
 
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(3,8,0)
 static int smo8800_remove(struct acpi_device *device)
-#else
-static int smo8800_remove(struct acpi_device *device, int type)
-#endif
 {
-	if (smo8800_dev.irq)
-		free_irq(smo8800_dev.irq, &smo8800_dev);
-	misc_deregister(&smo8800_dev.miscdev);
+	struct smo8800_device *smo8800 = device->driver_data;
+	free_irq(smo8800->irq, smo8800);
+	misc_deregister(&smo8800->miscdev);
+	dev_info(&device->dev, "device /dev/freefall unregistered\n");
 	return 0;
 }
 
-static const struct acpi_device_id acc_device_ids[] = {
+static const struct acpi_device_id smo8800_ids[] = {
 	{ "SMO8800", 0 },
 	{ "SMO8810", 0 },
 	{ "", 0 },
 };
 
-MODULE_DEVICE_TABLE(acpi, acc_device_ids);
+MODULE_DEVICE_TABLE(acpi, smo8800_ids);
 
-static struct acpi_driver latitude_acc_driver = {
-	.name =		DRIVER_NAME,
-	.class =	"Latitude",
-	.ids =		acc_device_ids,
-	.ops =		{
-		.add =		smo8800_add,
-		.remove =	smo8800_remove,
+static struct acpi_driver smo8800_driver = {
+	.name = DRIVER_NAME,
+	.class = "Latitude",
+	.ids = smo8800_ids,
+	.ops = {
+		.add = smo8800_add,
+		.remove = smo8800_remove,
 	},
-	.owner =	THIS_MODULE,
+	.owner = THIS_MODULE,
 };
 
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(3,6,0)
-module_acpi_driver(latitude_acc_driver);
-#else
-static int __init smo8800_init(void)
-{
-	int result = 0;
+module_acpi_driver(smo8800_driver);
 
-	result = acpi_bus_register_driver(&latitude_acc_driver);
-	if (result < 0) {
-		ACPI_DEBUG_PRINT((ACPI_DB_ERROR,
-				  "Error registering driver\n"));
-		return -ENODEV;
-	}
-
-	return 0;
-}
-
-static void __exit smo8800_exit(void)
-{
-	acpi_bus_unregister_driver(&latitude_acc_driver);
-}
-
-module_init(smo8800_init);
-module_exit(smo8800_exit);
-#endif
-
-MODULE_DESCRIPTION("Dell laptop freefall driver (ACPI " DRIVER_NAME ")");
+MODULE_DESCRIPTION("Dell Latitude freefall driver (ACPI SMO8800/SMO8810)");
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Sonal Santan");
+MODULE_AUTHOR("Sonal Santan, Pali Rohár");
